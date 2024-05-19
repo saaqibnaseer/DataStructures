fd95b9d27ed587fa8eb5a7fff1afd343e61c8841
Saqib <saqib.naseer@routetrading.co.uk>
2024-05-19 17:22:34 +0500

    NumIdenticalPairs LeetCode Problem

--- SearchingAlgorithms/ArrayADT.cs
+++ SearchingAlgorithms/ArrayADT.cs
@@ -14,17 +14,41 @@ namespace SearchingAlgorithms
         public static int[] Shuffle(int[] nums, int no)
         {
             int n = nums.Length;
-            int[] Answer = new int[no * 2];
+            int[] answer = new int[no * 2];
 
             int j = no;
             int k = 0;
 
             for (int i = 0; i < n; i++)
             {
-                Answer[i] = (i % 2 == 1) ? nums[j++] : nums[k++];
+                answer[i] = (i % 2 == 1) ? nums[j++] : nums[k++];
             }
 
-            return Answer;
+            return answer;
+        }
+
+        //Given an array of integers nums, return the number of good pairs.
+        //A pair(i, j) is called good if nums[i] == nums[j] and i<j.
+        public static int NumIdenticalPairs(int[] nums)
+        {
+            int goodPairCount = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+
+                    if (nums[i] == nums[j])
+                    {
+                        goodPairCount++;
+                    }
+
+                }
+
+            }
+
+            return goodPairCount;
         }
 
         static void Main(string[] args)

b74c1b956559beabdfbe31334bc7325f20b5ed3b
Saqib <saqib.naseer@routetrading.co.uk>
2024-05-25 21:25:14 +0500

    FindMissingElementOfArray

---
+++ ArrayExercies/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExercies
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentExercise.FindMissingElementOfArray();
+            Console.WriteLine("");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ArrayExercies/StudentExercise.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExercies
+{
+    public class StudentExercise
+    {
+        // Find the missing elements from a sorted integer array
+        public static void FindMissingElementOfArray()
+        {
+            int diff = 1;
+            int[] array = { 1, 2, 5, 6, 7, 9, 10, 11, 12, 13, 14 };     // Given Array
+
+            for (int index = 0; index < array.Length; index++)
+            {
+                if (array[index] - index != diff)
+                {
+                    while (diff < array[index] - index)
+                    {
+                        Console.WriteLine(index + diff++);
+                    }
+                }
+            }
+        }
+    }
+}
